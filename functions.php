9c357a3a9ecdd03758d6b0fc409473a6894b9faf
Chris <slackcc@gmail.com>
2015-02-21 18:34:06 -0500

    Changing mobile menu

--- functions.php
+++ functions.php
@@ -5,3 +5,11 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+//* Load Font Awesome
+add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
+function enqueue_font_awesome() {
+
+	wp_enqueue_style( 'font-awesome', '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
+
+}
